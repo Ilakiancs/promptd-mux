15f2187d1d94700ea823d10cd89962dc6d7908da
ilakiancs <ilakian15@gmail.com>
2025-08-27 23:42:31 +0530

    Initial Commit

---
+++ MenubarGPT/MenubarGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenubarGPTApp.swift
+//  MenubarGPT
+//
+//  Created by Ilakian Puvanendra on 2025-08-27.
+//
+
+import SwiftUI
+
+@main
+struct MenubarGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
